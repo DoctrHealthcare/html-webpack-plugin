7e68fea92672784ce228252b20b8d870e63768f7
Michal Stawicki <stawicki@gmail.com>
2016-12-08 10:40:50 +0100

    put styles in head

--- index.js
+++ index.js
@@ -497,7 +497,7 @@ HtmlWebpackPlugin.prototype.generateAssetTags = function (assets) {
     });
   }
   // Add styles to the head
-  body = body.concat(styles);
+  head = head.concat(styles);
   // Add scripts to body or head
   if (this.options.inject === 'head') {
     head = head.concat(scripts);
